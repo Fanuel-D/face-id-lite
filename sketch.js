15fa4c7126ce71aa9c498c3c52f0dcb2f351b366
Fanuel Dana <fdana1@cs.swarthmore.edu>
2024-04-28 14:27:53 -0400

    face detection partially working

--- sketch.js
+++ sketch.js
@@ -1,121 +1,129 @@
-// const video = document.getElementById("video");
-let webcam;
-let modelsReady = false;
-// let totalModels = 3;
+// let webcam;
+// let modelsReady = false;
+// let faceCanvas;
+// let displaySize
 
-function preload(){
-  faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models');
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models');
-  modelsReady = true;
-}
 
-// function modelLoaded() {
-//   totalModels--;
-//   if (totalModels==0) {
-//     modelsReady = True;
+// async function preload(){
+//   await Promise.all([
+//     faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+//     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+//     faceapi.nets.faceLandmark68Net.loadFromUri('/models')
+//   ]);
+//   modelsReady = true;
+// }
+
+// function setup() {
+//   createCanvas(640, 480);
+//   webcam = createCapture(VIDEO);
+//   webcam.size(width, height);
+//   webcam.hide();
+// }
+
+// async function getLabeledFaceDescriptions() {
+//   const labels = ["Maxwell", "Messi", "Fanuel"];
+//   const labeledDescriptors = [];
+
+//   for (const label of labels) {
+//     let descriptions = [];
+//     for (let i = 1; i <= 2; i++) {
+//       const img = await faceapi.fetchImage(`./labels/${label}/${i}.png`);
+//       const detection = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
+//       if (detection) {
+//         descriptions.push(detection.descriptor);
+//       } else {
+//         console.error('No face detected in image', label, i);
+//       }
+//     }
+//     labeledDescriptors.push(new faceapi.LabeledFaceDescriptors(label, descriptions));
+//   }
+//   return labeledDescriptors;
+// }
+
+
+
+// function draw(){
+//   if (modelsReady){
+//     image(webcam,0,0,width,height);
+//     detectAndDrawFaces();
+//   }
+
+// }
+
+
+// async function detectAndDrawFaces() {
+//   try {
+//     const labeledFaceDescriptors = await getLabeledFaceDescriptions();
+//     const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
+//     faceCanvas = faceapi.createCanvasFromMedia(webcam.elt);
+//     displaySize = { width: width, height: height };
+//     faceapi.matchDimensions(faceCanvas, displaySize);
+
+
+//     setInterval(async () => {
+//       const detections = await faceapi
+//         .detectAllFaces(webcam.elt)
+//         .withFaceLandmarks()
+//         .withFaceDescriptors();
+  
+//       const resizedDetections = faceapi.resizeResults(detections, displaySize);
+  
+//       faceCanvas.getContext("2d").clearRect(0, 0, faceCanvas.width, faceCanvas.height);
+  
+//       const results = resizedDetections.map((d) => {
+//         return faceMatcher.findBestMatch(d.descriptor);
+//       });
+//       results.forEach((result, i) => {
+//         const box = resizedDetections[i].detection.box;
+//         const drawBox = new faceapi.draw.DrawBox(box, {
+//           label: result,
+//         });
+//         drawBox.draw(canvas);
+//       });
+//     }, 100);
+
+//     image(faceCanvas, 0, 0);
+//   } catch (error) {
+//     console.error('Failed to detect and draw faces:', error);
 //   }
 // }
-async function getLabeledFaceDescriptions() {
-  const labels = ["Maxwell", "Messi"];
-  const labeledDescriptors = [];
-
-  for (const label of labels) {
-    let descriptions = [];
-    for (let i = 1; i <= 2; i++) {
-      const img = await faceapi.fetchImage(`./labels/${label}/${i}.png`);
-      const detection = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
-      if (detection) {
-        descriptions.push(detection.descriptor);
-      } else {
-        console.error('No face detected in image', label, i);
-      }
-    }
-    labeledDescriptors.push(new faceapi.LabeledFaceDescriptors(label, descriptions));
-  }
-  return labeledDescriptors;
-}
-
+let video;
+let detections = [];
 
 function setup() {
-  createCanvas(640, 480);
-  webcam = createCapture(VIDEO);
-  webcam.size(width, height);
-  webcam.hide();
+    createCanvas(640, 480);
+    video = createCapture(VIDEO);
+    video.size(width, height);
+    video.hide();
+
+    // Load models with full path to model directory
+    Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('/models')
+    ]).then(initDetect); // Ensure all models are loaded before starting detection
 }
 
-
-
-function draw(){
-  if (modelsReady){
-    image(webcam,0,0,width,height);
-  }
-
+function initDetect() {
+    console.log("Models loaded successfully!");
 }
 
-// Function to load and process face descriptions for a single label
-// function loadFaceDescriptions(label) {
-//   let descriptions = [];
-//   for (let i = 1; i <= 2; i++) {
-//       let img = faceapi.fetchImage(`./labels/${label}/${i}.png`);
-//       let detection = faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
-//       if (detection) {
-//           descriptions.push(detection.descriptor);
-//       } else {
-//           console.error('No face detected in image', label, i);
-//       }
-//   }
-//   return new faceapi.LabeledFaceDescriptors(label, descriptions);
-// }
+async function draw() {
+    image(video, 0, 0, width, height);
 
-// Main function to process all labels individually
-// function getLabeledFaceDescriptions() {
-//   let labels = ["Maxwell", "Messi"];
-//   let descriptors = [];
+    detections = await faceapi.detectAllFaces(video.elt, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
 
-//   for (let label of labels) {
-//       let descriptor = loadFaceDescriptions(label);
-//       descriptors.push(descriptor);
-//   }
+    if (detections && detections.length > 0) {
+        detections.forEach(det => {
+            if (det && det.detection && det.detection.box) {
+                const { x, y, width, height } = det.detection.box;
+                stroke(255, 0, 0);
+                strokeWeight(2);
+                noFill();
+                rect(x, y, width, height);
+            }
+        });
+    }
+}
 
-//   return descriptors;
-// }
-
-
-
-
-// video.addEventListener("play", async () => {
-//   const labeledFaceDescriptors = await getLabeledFaceDescriptions();
-//   const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
-
-//   const canvas = faceapi.createCanvasFromMedia(video);
-//   document.body.append(canvas);
-
-//   const displaySize = { width: video.width, height: video.height };
-//   faceapi.matchDimensions(canvas, displaySize);
-
-//   setInterval(async () => {
-//     const detections = await faceapi
-//       .detectAllFaces(video)
-//       .withFaceLandmarks()
-//       .withFaceDescriptors();
-
-//     const resizedDetections = faceapi.resizeResults(detections, displaySize);
-
-//     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-
-//     const results = resizedDetections.map((d) => {
-//       return faceMatcher.findBestMatch(d.descriptor);
-//     });
-//     results.forEach((result, i) => {
-//       const box = resizedDetections[i].detection.box;
-//       const drawBox = new faceapi.draw.DrawBox(box, {
-//         label: result,
-//       });
-//       drawBox.draw(canvas);
-//     });
-//   }, 100);
-// });
-
-//take pics and save them locally then use them as the  input for the mode
 
