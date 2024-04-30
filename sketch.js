bbc3c9b9eada80d3bb600d2c61062a216a720b38
Fanuel Dana <fdana1@cs.swarthmore.edu>
2024-04-30 14:03:02 -0400

    sobel filter progress

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 let webcam;
-let displaySize;
 let readyToDetect;
 let faceMatcher;
-
-
-
 let video;
+let temp;
 let detections = [];
+let threshold = 128;
+
+const sobelH = [[-1,0,1],[-2,0,2],[-1,0,1]]
+const sobelV = [[-1,-2,-1],[0,0,0],[1,2,1]]
 
 function setup() {
     createCanvas(640, 480);
@@ -27,8 +28,9 @@ function initDetect() {
     console.log("Models loaded successfully!");
     initFaceMatcher();
 }
+
 async function loadLabeledImages() {
-  const labels = ["Fanuel", "Maxwell","messi"]; 
+  const labels = ["Fanuel","Maxwell","messi"]; 
   return Promise.all(
       labels.map(async label => {
           const descriptions = [];
@@ -41,6 +43,7 @@ async function loadLabeledImages() {
       })
   );
 }
+
 async function initFaceMatcher() {
     const labeledFaceDescriptors = await loadLabeledImages();
     faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
@@ -48,34 +51,116 @@ async function initFaceMatcher() {
     readyToDetect = true;  // Set a flag indicating that faceMatcher is ready
 }
 
+
+
+function convolution(x, y, matrix, matrixsize, img) {
+    let rtotal = 0.0;
+    let gtotal = 0.0;
+    let btotal = 0.0;
+    const offset = Math.floor(matrixsize / 2);
+    for (let i = 0; i < matrixsize; i++) {
+      for (let j = 0; j < matrixsize; j++) {
+        // What pixel are we testing
+        const xloc = x + i - offset;
+        const yloc = y + j - offset;
+        let loc = (xloc + img.width * yloc) * 4;
+  
+        // Make sure we haven't walked off our image, we could do better here
+        loc = constrain(loc, 0, img.pixels.length - 1);
+  
+        // Calculate the convolution
+        // retrieve RGB values
+        rtotal += img.pixels[loc] * matrix[i][j];
+        gtotal += img.pixels[loc + 1] * matrix[i][j];
+        btotal += img.pixels[loc + 2] * matrix[i][j];
+      }
+    }
+    // Make sure RGB is within range
+    rtotal = constrain(Math.abs(rtotal), 0, 255);
+    gtotal = constrain(Math.abs(gtotal), 0, 255);
+    btotal = constrain(Math.abs(btotal), 0, 255);
+  
+    // Return the resulting color
+    return color(rtotal, gtotal, btotal);
+  }
+
+
+function blur(matrix, edgeImg, matrixsize){
+    let brightCount = 0;
+    let darkCount = 0;
+    edgeImg.loadPixels();
+    for (let x = 1; x < edgeImg.width; x++) {
+      for (let y = 1; y < edgeImg.height; y++) {
+        let c = convolution(x, y, matrix, matrixsize, edgeImg);
+        let intensity = (red(c) + green(c) + blue(c)) / 3;
+  
+        // retrieve the RGBA values from c and update pixels()
+        let loc = (x + y * edgeImg.width) * 4;
+
+
+        if (intensity >= threshold) {
+                // Set to bright (white)
+          edgeImg.pixels[loc] = 255;
+          edgeImg.pixels[loc + 1] = 255;
+          edgeImg.pixels[loc + 2] = 255;
+          brightCount++;
+        } else {
+                // Set to dark (black)
+          edgeImg.pixels[loc] = 0;
+          edgeImg.pixels[loc + 1] = 0;
+          edgeImg.pixels[loc + 2] = 0;
+          darkCount++;
+        }
+        edgeImg.pixels[loc + 3] = 255; 
+      }
+    }
+
+    edgeImg.updatePixels();
+    return {brightCount, darkCount}
+  }
+
+  
+  
+  
+
 async function draw() {
+
+    temp = video.get();
+    let counts = blur(sobelV,temp,3);
+
+
+
+
 
     if (!readyToDetect) {
         console.log("Models not ready");
         return;
     }
-  image(video, 0, 0, width, height);
 
-  if (readyToDetect){
 
-        const detections = await faceapi.detectAllFaces(video.elt, new faceapi.SsdMobilenetv1Options())
-            .withFaceLandmarks().withFaceDescriptors();
+    if (readyToDetect){
 
-    if (detections) {
-        const resizedDetections = faceapi.resizeResults(detections, {width, height});
-        resizedDetections.forEach(det => {
-            const match = faceMatcher.findBestMatch(det.descriptor);
-            stroke(0, 255, 0);
-            strokeWeight(2);
-            noFill();
-            rect(det.detection.box.x, det.detection.box.y, det.detection.box.width, det.detection.box.height);
-            textSize(20);
-            fill(255);
-            text(match.toString(), det.detection.box.x, det.detection.box.y);
-        });
-  }
+      const detections = await faceapi.detectAllFaces(video.elt, new faceapi.SsdMobilenetv1Options())
+          .withFaceLandmarks().withFaceDescriptors();
+          
+      if (detections) {
+          const resizedDetections = faceapi.resizeResults(detections, {width, height});
+          resizedDetections.forEach(det => {
+              const match = faceMatcher.findBestMatch(det.descriptor);
+              if(counts.brightCount >= 270000 && counts.brightCount <= 290000 && counts.darkCount >= 22000 && counts.darkCount <= 25000){
+                stroke(0, 255, 0);
+                strokeWeight(2);
+                noFill();
+                rect(det.detection.box.x, det.detection.box.y, det.detection.box.width, det.detection.box.height);
+                textSize(20);
+                fill(255);
+                text(match.toString(), det.detection.box.x, det.detection.box.y);
+              }
+          });
+    }
 
-  }
+    }
+    
   
 }
 
